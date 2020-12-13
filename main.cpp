a072ee3b24ae9900ba56ffc6b42a6873f21ab55b
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2020-12-13 20:51:26 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <map>
+
+using namespace std;
+
+void file(const string FILE = "Test")
+{
+    freopen((FILE + ".INP").c_str(), "r", stdin);
+    freopen((FILE + ".OUT").c_str(), "w", stdout);
+}
+
+char __;
+template<typename T>
+void getUnsign(T &x)
+{
+    while (__ = getchar(), __ < '0' || __ > '9');
+ 
+    x = (__ - '0');
+    while (__ = getchar(), __ >= '0' && __ <= '9')
+        x = (x << 3) + (x << 1) + (__ - '0');
+}
+
+template<typename T>
+void getSigned(T &x)
+{
+    while (__ = getchar(), __ != '-' && (__ < '0' || __ > '9'));
+    bool sign(__ == '-');
+    if (sign) __ = getchar();
+ 
+    x = (__ - '0');
+    while (__ = getchar(), __ >= '0' && __ <= '9')
+        x = (x << 3) + (x << 1) + (__ - '0');
+
+    if (sign) x = -x;
+}
+
+template<typename T>
+void getString(T &s)
+{
+    while (__ = getchar(), __ < 'a' || __ > 'z');
+
+    s.assign(1, __);
+    while (__ = getchar(), __ >= 'a' && __ <= 'z')
+        s += __;
+}
+
+template<typename T> void maximize(T &res, const T &val) { if (res < val) res = val; }
+template<typename T> void minimize(T &res, const T &val) { if (res > val) res = val; }
+
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+typedef pair<int, int> pi;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+/// ====*====*====*====*====*====*====*====*====*====*====*====*====*====*====*====
+
+int n, k;
+int expect;
+ll magic(int i = 0, int mask = 0)
+{
+    if (i >= n) return (mask == expect);
+
+    ll res = 0;
+    for (int j = 0; j < k; ++j)
+        res += magic(i + 1, mask | (1 << j));
+    
+    return res;
+}
+
+vector<ll> fact = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000};
+int brute()
+{
+    expect = (1 << k) - 1;
+    cout << magic() / fact[k] << '\n';
+}
+
+int main()
+{
+    int q;
+    cin >> q;
+
+    for (n = 1; n <= 10; ++n)
+    {
+        for (k = 1; k <= n; ++k)
+        {
+            
+        }
+    }
+    return 0;
+}
